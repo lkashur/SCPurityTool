52691d583217c0c7a663b74a720962a10762d3c7
Michael Mooney <mrmooney@colostate.edu>
2020-10-29 12:41:57 -0600

    Update to python script

--- raw_to_root.py
+++ raw_to_root.py
@@ -9,17 +9,10 @@ import numpy as np
 
 def main(argv):
     inputfile = ''
-    outputfile = ''
-    try:
-        opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
-    except getopt.GetoptError:
-        print 'test.py -i <inputfile> -o <outputfile>'
-        sys.exit(2)
+    outputfile = 'output.root'
+    opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
     for opt, arg in opts:
-        if opt == '-h':
-            print 'test.py -i <inputfile> -o <outputfile>'
-            sys.exit()
-        elif opt in ("-i", "--ifile"):
+        if opt in ("-i", "--ifile"):
             inputfile = arg
         elif opt in ("-o", "--ofile"):
             outputfile = arg
