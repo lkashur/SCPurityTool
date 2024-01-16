377a9b95739c5f4690b569f34c8df111fa069c5d
Lane Kashur <lkashur@gmail.com>
2024-01-15 21:18:43 -0600

    Simplify transfer of packet data to ROOT file

--- raw_to_root.py
+++ raw_to_root.py
@@ -1,59 +1,32 @@
-#!/usr/bin/python
-
-import sys, getopt
-import ROOT
-from ROOT import TFile, TTree, gROOT, AddressOf
-from root_numpy import array2tree, root2array
-import h5py
+# imports
 import numpy as np
+import pandas as pd
+import h5py
+import ROOT
+import argparse
 
-def main(argv):
-    inputfile = ''
-    outputfile = 'output.root'
-    opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
-    for opt, arg in opts:
-        if opt in ("-i", "--ifile"):
-            inputfile = arg
-        elif opt in ("-o", "--ofile"):
-            outputfile = arg
-
-    #select raw data file
-    f = h5py.File(inputfile, 'r')
-	
-    #check for "corrupt" packets
+def main(args):
+    
+    # Load data and store (temporarily) in a pandas dataframe
+    f = h5py.File(args.in_file, 'r')
     data_mask = f['packets'][:]['packet_type'] == 0
     valid_parity_mask = f['packets'][data_mask]['valid_parity'] == 1
-    good_data = (f['packets'][data_mask])[valid_parity_mask]
-	
-    #relevant packet info
-    chip_id = good_data['chip_id'].astype(np.uint64)
-    channel_id = good_data['channel_id'].astype(np.uint64)
-    timestamps = good_data['timestamp']
-    adcs = good_data['dataword']
-    
-    #placing relevant data in np arrays
-    adcs_array = np.array(adcs)
-    adcs_array.dtype = [('adc_counts', 'uint8')]
+    data = (f['packets'][data_mask])[valid_parity_mask]
+    df = pd.DataFrame(np.array(data))
 
-    timestamps_array = np.array(timestamps)
-    timestamps_array.dtype = [('timestamp', 'uint64')]
-	
-    chipids_array = np.array(chip_id)
-    chipids_array.dtype = [('chip_id', 'uint64')]
-	
-    channel_ids_array = np.array(channel_id)
-    channel_ids_array.dtype = [('channel_id', 'uint64')]
-	
-    #ROOT
-    f = TFile(outputfile, 'RECREATE')
-	
-    #fill TTree
-    tree = array2tree(adcs_array)
-    array2tree(timestamps_array, tree=tree)
-    array2tree(chipids_array, tree=tree)
-    array2tree(channel_ids_array, tree=tree)
-	
-    tree.Write()
+    # Save to ROOT TTree                                                                                        
+    chip_id = df['chip_id'].to_numpy().astype('int32')                                                                                                 
+    channel_id = df['channel_id'].to_numpy().astype('int32')            
+    timestamp = df['timestamp'].to_numpy().astype('int32')
+    dataword = df['dataword'].to_numpy().astype('int32')
+    data_dict = {'chip_id': chip_id, 'channel_id': channel_id, 'timestamp': timestamp, 'dataword': dataword}                                 
+    rdf = ROOT.RDF.FromNumpy(data_dict)                                                                                                                
+    rdf.Snapshot('tree', args.out_file)                                                                                                    
     
-if __name__ == "__main__":
-    main(sys.argv[1:])
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--in_file', required=True)
+    parser.add_argument('--out_file', required=True)
+    args = parser.parse_args()
+    main(args)
